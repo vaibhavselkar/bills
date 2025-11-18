af9fe75c46bbcf5268a9a9abbd51ab7d0e6a29cb
Apeksha <134759404+Apekshagarse@users.noreply.github.com>
2025-11-18 10:33:42 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import ProductManager from "./components/ProductManager";
 import Analytics from './components/Analytics';
 import AdminAnalytics from './components/AdminAnalytics';
 import AdminDashboard from './components/AdminDashboard';
-import Tables from './components/Tables';
+import Tables from './components/TopSells.js';
 import Login from './components/Login.js';
 import Register from './components/Register';
 import { ForgetPassword, ResetPassword } from "./components/aa";
@@ -15,27 +15,33 @@ import Logout from "./components/Logout";
 import UserTable from "./components/UserTable";
 import UserBill from "./components/UserBill";
 import EachUserBill from "./components/EachUserBill";
+import Occasion from './components/Occasion';
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Public routes without sidebar */}
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/bill" element={<BillForm />} />
-        <Route path="/view" element={<ViewBills />} />
-        <Route path="/user-bills" element={<UserBill />} />
-        <Route path="/each-bills/:userId" element={<EachUserBill />} />
-        <Route path="/invoice/:id" element={<Invoice />} />
-        <Route path="/products" element={<ProductManager />} /> {/*use for commenting*/}
-        <Route path="/analytics" element={<Analytics />} />
-        <Route path="/admin-analytics" element={<AdminAnalytics />} />
-        <Route path="/tables" element={<Tables />} />
         <Route path="/forgot-password" element={<ForgetPassword />} />
         <Route path="/reset-password/:token" element={<ResetPassword />} />
-        <Route path="/user-dashboard" element={<UserDashboard />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />       
         <Route path="/logout" element={<Logout />} />
+        <Route path="/invoice/:id" element={<Invoice />} />
+
+        {/* User routes without sidebar */}
+        <Route path="/user-dashboard" element={<UserDashboard />} />
+        <Route path="/bill" element={<BillForm />} />
+        <Route path="/analytics" element={<Analytics />} />
+        <Route path="/user-bills" element={<UserBill />} />
+        <Route path="/each-bills/:userId" element={<EachUserBill />} />
+        <Route path="/occasion" element={<Occasion />} />
+        <Route path="/products" element={<ProductManager />} />
+    
+        <Route path="/view" element={<ViewBills />} />
+        <Route path="/admin-analytics" element={<AdminAnalytics />} />
+        <Route path="top-sells" element={<Tables />} /> 
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />       
         <Route path="/users" element={<UserTable />} />
       </Routes>
     </Router>
