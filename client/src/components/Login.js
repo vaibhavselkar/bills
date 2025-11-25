10a7074792f976097e67dbfbe704133a0d112f06
Vaibhav Selkar <132983753+vaibhavselkar@users.noreply.github.com>
2025-11-25 13:20:29 +0530

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8080/api/user/login', {
+      const response = await fetch('https://bills-weld.vercel.app/api/user/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
