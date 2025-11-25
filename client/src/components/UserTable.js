a2b7f81e87ab63336711990432781ce088781244
Vaibhav Selkar <132983753+vaibhavselkar@users.noreply.github.com>
2025-11-25 15:18:01 +0530

    Update UserTable.js

--- client/src/components/UserTable.js
+++ client/src/components/UserTable.js
@@ -21,7 +21,7 @@ const UserTable = () => {
     const fetchUsers = async () => {
       try {
         const token = localStorage.getItem("token"); // assume stored at login
-        const res = await fetch("http://localhost:8080/api/user/users", {
+        const res = await fetch("https://bills-weld.vercel.app/api/user/users", {
           headers: {
             Authorization: `Bearer ${token}`,
           },
