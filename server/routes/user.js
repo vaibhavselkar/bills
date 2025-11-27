f6c4286da8f3b1afa1a2916721fa8c1442a1fdad
Vaibhav Selkar <132983753+vaibhavselkar@users.noreply.github.com>
2025-11-27 13:33:33 +0530

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -23,7 +23,13 @@ router.post('/login', async (req, res) => {
       return res.status(400).json({ message: 'Invalid credentials' });
 
     const token = jwt.sign(
-      { id: user._id, role: user.role },
+      { { 
+        id: user._id, 
+        role: user.role,
+        tenantId: user.tenantId,
+        name: user.name,
+        email: user.email
+      }, },
       process.env.JWT_SECRET || 'mysecret',
       { expiresIn: '2h' }
     );
